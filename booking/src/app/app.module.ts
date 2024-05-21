4f87f0712ce9e2a3db8b54bc76d45c3e6ebbeec4
Andrea Borelli <andreaborelli@blu.it>
2024-05-21 14:42:36 +0200

    add server rest, comunic server, model, form, comp card e pipe, img gallery, forms, maps, rate...

--- booking/src/app/app.module.ts
+++ booking/src/app/app.module.ts
@@ -6,16 +6,22 @@ import { AppComponent } from './app.component';
 import { SearchComponent } from './features/search/search.component';
 import { LoginComponent } from './features/login/login.component';
 import { CartComponent } from './features/cart/cart.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { IntToArrayPipe } from './shared/pipes/int-to-array.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
     LoginComponent,
-    CartComponent
+    CartComponent,
+    IntToArrayPipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- booking/src/app/features/search/search.component.ts
+++ booking/src/app/features/search/search.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Hotel } from 'src/app/model/hotel';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
 
+  text = "Milan";
+  hotels: Hotel[] = []; // Array per memorizzare i risultati della ricerca
+  active: Hotel | undefined; // Variabile per memorizzare l'hotel attivo
+  activeImage: string | undefined; // Variabile per memorizzare l'immagine attiva
+
+  
+
+  constructor(private http: HttpClient) {
+    // Il costruttore inietta il servizio HttpClient
+  }
+
+  ngOnInit() {
+    // Metodo di ciclo di vita di Angular chiamato quando il componente viene inizializzato
+    const savedHotels = localStorage.getItem('hotels'); // Recupera i dati dei risultati salvati dal localStorage
+    if (savedHotels) {
+      // Se i dati esistono nel localStorage
+      this.hotels = JSON.parse(savedHotels); // Parsea i dati JSON e li assegna all'array hotels
+    } else {
+      // Se i dati non esistono nel localStorage
+      this.searchHotels(this.text); // Esegue una ricerca predefinita con il testo "Milan"
+    }
+  }
+
+  searchHotels(text: string) {// Metodo per cercare hotel basato sul testo fornito
+    this.text = text; // Assegna il testo fornito alla variabile text
+    this.http.get<Hotel[]>(`http://localhost:3000/hotels?city=` + text) // Effettua una richiesta HTTP GET all'URL specificato e ottiene un array di oggetti Hotel
+      .subscribe(result => { // Iscrizione all'osservabile per ottenere i risultati della richiesta
+        this.hotels = result; // Assegna i risultati all'array hotels
+        this.setActive(this.hotels[0]); // Imposta il primo hotel come attivo
+        localStorage.setItem('hotels', JSON.stringify(this.hotels)); // Salva i risultati nel localStorage come stringa JSON
+        console.log(this.hotels); // Stampa i risultati nella console
+      });
+  }
+  setActive(hotel: Hotel) { // Metodo per impostare l'hotel attivo
+    this.active = hotel; // Assegna l'hotel fornito alla variabile active
+    // console.log(hotel); // Stampa l'hotel nella console
+    this.activeImage = hotel.images[0]; // Imposta la prima immagine dell'hotel come immagine attiva
+  }
+
+  sendEmail({email, msg}: {email: string, msg: string}) { // Metodo per inviare un'email all'hotel attivo
+    // console.log('Sending email to: ', email, msg, this.active.email); // Stampa l'email nella console
+    window.alert(`sent:
+    ${email}
+    ${msg}
+    ${this.active!.email}`);// {} espressione di template string}
+  }
 }
